a9a7d7e95c132e1ef656ab3224b4ac6fee700911
Kiwis01 <68796207+Kiwis01@users.noreply.github.com>
2023-07-14 14:55:14 -0700

    Delete tempCodeRunnerFile.js

--- tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-const submitButton = document.querySelector('#submit');
-// const outPutElement = document.querySelector('#output');
-// const inputElement = document.querySelector('input');
-// const hiddenElements = document.querySelectorAll('.hidden');
